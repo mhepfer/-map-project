97a0163cb70fd78204b77d4c2375717a4085f86b
Travis Herrick <quasimonomial@gmail.com>
2020-06-02 13:02:18 -0700

    clean up data with a map function instead of reassignment

--- js/init.js
+++ js/init.js
@@ -2,12 +2,11 @@ var init = async function() {
 	// these can happen at the same time
 	drawMap(map_defaults.map_w, map_defaults.map_h, map_defaults.map_margin);
 	dataset = await load_clean_data("data/clean-lewis-and-clark-data.csv");
+
 	// but these have to happen after the above two
 	populate_map();
 	addPeopleLegend();
 	initTimeline();
-	// add timeline
-	// add people
 }
 
 init();
